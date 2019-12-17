765f55f2e084d3b8d4945ebfde73235915ea5d1f
Bernat <Bernat@DESKTOP-9H77DBT>
2019-12-17 13:49:40 +0100

    el pie dinamic pareix q funciona, falta controlar les variables de sessio?

--- web/p_privada/nuvol.js
+++ web/p_privada/nuvol.js
@@ -1,64 +1,75 @@
 
 var nuvol_base_url = "http://localhost:8080/PeliculesWeb/bdpeliculas?op=numpelisdepersona&par=";
-//clau: nom de l'actor, valor: num de pelis
+
+//dos arrays mapejats (array key value no aconseguesc q funcioni)
 var nuvol_actors = [];
+var nuvol_pelis = [];
+
 $(window).on('hashchange', function (e) {
     console.log(nuvol_actors);
     //agafam el valor hash i li llevam el hashtag
     actor = window.location.hash;
     actor = actor.substring(1, actor.length);
     console.log(actor);
-    //afegim l'actor pero no sabem el nombre de pelis encara
-    nuvol_actors[actor] = "?";
+    //afegim només l'actor pq no sabem el nombre de pelis encara
+    nuvol_actors.push(actor);
     nuvol_llegirGrafica(actor, nuvol_base_url + actor);
 });
+
 function nuvol_llegirGrafica(actor, url) {
     result = sessionStorage.getItem(actor);
     if (result === null) {
         $.ajax({url: url,
             success: function (result) {
                 sessionStorage.setItem(actor, result);
-                nuvol_actors[actor] = parseInt(result.substring(result.indexOf("[") + 1, result.indexOf("]")));
+                nuvol_pelis.push(parseInt(result.substring(result.indexOf("[") + 1, result.indexOf("]"))));
                 nuvol_pie();
             }});
     } else {
-        nuvol_actors[actor] = parseInt(result.substring(result.indexOf("[") + 1, result.indexOf("]")));
+        nuvol_pelis.push(parseInt(result.substring(result.indexOf("[") + 1, result.indexOf("]"))));
         nuvol_pie();
     }
 
 }
 
-
 function nuvol_pie() {
-    var obj = new Object();
-    obj.chart = {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        borderRadius: 10,
-        type: 'pie'
-    };
-    obj.title = '';
-    obj.tooltip = {pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'};
-    obj.plotOptions = {
-        nuvol: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            }
-        }
-    };
-    obj.series = [];
-    for (var i = 0; i < nuvol_actorns.length; i++) {
-        obj.series.push({
-            name: i,
-            y: nuvol_actorns[i],
-        })
+    $('#espera_pie').empty();
+    $('#pie').empty();
+    variables = "[";
+    for (i = 0; i < nuvol_actors.length; i++) {
+        variables = variables + "{\"name\": \"" + nuvol_actors[i] + "\", \"y\": " + nuvol_pelis[i] + "},";
     }
+    variables = variables.substring(0, variables.length - 1) + "]";
+    variables=variables.replace('%20',' ');
 
-   var jsonPie = JSON.stringify(obj);
-    console.log(jsonPie);
-    $('#pie').highcharts(jsonPie);
+    Highcharts.chart('pie', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            borderRadius: 10,
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+                name: 'Pelis per actor',
+                colorByPoint: true,
+                data: JSON.parse(variables)
+            }]
+    });
 }
